33df66d76bd1759125f1fd6000041b67c154e2e3
jccastillofeutech <ultraorenone@gmail.com>
2025-11-29 20:10:33 +0800

    Fixed firebase errors

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,14 +1,17 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCeSnm2Dkevz8nto49EOG-y7jOOMHlAnoM",
-    authDomain: "moodtree-bdc88.firebaseapp.com",
-    projectId: "moodtree-bdc88",
-    storageBucket: "moodtree-bdc88.firebasestorage.app",
-    messagingSenderId: "721781735896",
-    appId: "1:721781735896:web:279be28f1166f7cb6732ad",
-    measurementId: "G-DLFEQ9SDMJ"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-  const analytics = firebase.analytics ? firebase.analytics() : null;
+const firebaseConfig = {
+  apiKey: "AIzaSyCeSnm2Dkevz8nto49EOG-y7jOOMHlAnoM",
+  authDomain: "moodtree-bdc88.firebaseapp.com",
+  projectId: "moodtree-bdc8d",
+  storageBucket: "moodtree-bdc8d.appspot.com",
+  messagingSenderId: "721781735896",
+  appId: "1:721781735896:web:279be28f1166f7cb6732ad",
+  measurementId: "G-DLFEQ9SDMJ"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
